37f1bb67de0071a9b562c0efbdc673b7246c9ccd
eeddaann <edan100@gmail.com>
2018-01-18 00:50:55 +0200

    workarounds for openshift

--- blockchain.py
+++ blockchain.py
@@ -40,8 +40,8 @@ class Blockchain:
 
         while current_index < len(chain):
             block = chain[current_index]
-            print(f'{last_block}')
-            print(f'{block}')
+            print('{'+last_block+'}')
+            print('{'+block+'}')
             print("\n-----------\n")
             # Check that the hash of the block is correct
             if block['previous_hash'] != self.hash(last_block):
@@ -72,7 +72,7 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            response = requests.get('http://'+node+'/chain')
 
             if response.status_code == 200:
                 length = response.json()['length']
@@ -348,7 +348,7 @@ def new_transaction():
     # Create a new Transaction
     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-    response = {'message': f'Transaction will be added to Block {index}'}
+    response = {'message': 'Transaction will be added to Block '+str(index)}
     return jsonify(response), 201
 
 
